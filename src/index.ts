2189a41882749443ed64d641f4bd0e772d92a527
caja_autoservicio <caja.prat@cademartori.cl>
2025-10-06 16:12:29 -0300

    add test route dinamic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { node } from '@elysiajs/node'
 import { PORT } from '../config.js';
 import type { server } from './types/index.js';
@@ -7,7 +7,28 @@ const app = new Elysia({ adapter: node() });
 
 app.get('/', () => test)
 
+app.get('/test', () => {
+    return new Response('<h1>Test route</h1>', {
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8'
+        }
+    })
+})
+
+app.get('/api/test/:id', ({ params }) => {
+    const foundItem = test.find(item => item.id === params.id);
+    return foundItem ?? { error: 'Not found' };
+}, {
+    params: t.Object({
+        id: t.Number()
+    })
+})
+
+
+
+
 app.listen(PORT, ({ hostname, port }: server) => console.log(`Server running at http://${hostname}:${port}`))
+
 
 
 
