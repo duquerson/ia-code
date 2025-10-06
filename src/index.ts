610cba3e6402a1b776a6eb962a29aca7009e68b1
caja_autoservicio <caja.prat@cademartori.cl>
2025-10-06 12:38:45 -0300

    add mockus

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { Elysia } from 'elysia'
 import { node } from '@elysiajs/node'
 import { PORT } from '../config.js';
 import type { server } from './types/index.js';
+import { test } from './mock/index.js';
 const app = new Elysia({ adapter: node() });
 
-app.get('/', () => 'Hello World!')
+app.get('/', () => test)
 
 app.listen(PORT, ({ hostname, port }: server) => console.log(`Server running at http://${hostname}:${port}`))
 
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+const test = [
+    {
+        "id": 1,
+        "content": "Add app.listen to start the server and log the URL",
+        "date": "2024-10-05T17:30:31.000Z",
+        "important": "false",
+    },
+    {
+        "id": 2,
+        "content": "Fix import paths for PORT and server type",
+        "date": "2024-10-05T17:32:45.000Z",
+        "important": "false",
+    }
+];
+
+export { test };
